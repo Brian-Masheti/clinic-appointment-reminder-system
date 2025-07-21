ea062a42dcc16e4613b28de91263a1af347a052d
Brian Masheti <Brianxmasheti@gmail.com>
2025-07-21 04:34:56 +0300

    helmet for secure HTTP headers, morgan for logging, and has a robust global error handle

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
 
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
 
